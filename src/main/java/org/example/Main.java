74ca02762a4b1292821ae3da90aea744e852ef47
ToshithR <toshithr04@gmail.com>
2026-02-28 16:17:36 +0530

    Initial test

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Main {
+
+    @Test
+    public void login(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.salesforce.com/form/signup/elf-v2-login/");
+        }
+    }
